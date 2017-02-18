48d03049ee8f29191c5bb2b29a6e6694f4d1be99
Matthew <mdombro3@gmail.com>
2017-02-18 16:18:25 -0500

    Text sanitization

--- Google API/Gv_Test.py
+++ Google API/Gv_Test.py
@@ -16,9 +16,7 @@ def make_image_data_list(image_filenames):
         needs them to be
     """
     img_requests = []
-    print(image_filenames)
     for imgname in image_filenames:
-        print(imgname)
         with open(imgname, 'rb') as f:
             ctxt = b64encode(f.read()).decode()
             img_requests.append({
@@ -43,11 +41,46 @@ def request_ocr(api_key, image_filenames):
                              headers={'Content-Type': 'application/json'})
     return response
 
+def hasRandom(word):
+    if len(word) == 0 or word.isspace():
+        return False
+    letters = 'qwertyuiopasdfghjklmnbvcxzQWERTYUIOPASDFGHJKLMNBVCX'
+    numbers = '1234567890'
+    random = '@#$%^&*()+_=\|]}[{;:>,</.!?}"'
+    word_ = word[0:len(word)-1]
+    if len(word_) > 0:
+        prev_letter = word_[0]
+    for letter in word_:
+        if letter in random:
+            return True
+        if (prev_letter in letters and letter in numbers) or (prev_letter in numbers and letter in letters):
+            return True
+        prev_letter = letter
+    # . ! ? are ok at end as punctuation
+    if word[-1] in random[0:24]:
+        return True
+    return False
+
+def hasLetter(word):
+    letters = 'qwertyuiopasdfghjklmnbvcxzQWERTYUIOPASDFGHJKLMNBVCX1234567890'
+    for letter in word:
+        if letter not in letters:
+            return False
+    return True
+
+def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
+
+# Creates global dictionary variable
+with open("dict.txt") as word_file:
+    english_words = set(word.strip().lower() for word in word_file)
+
+def is_word(word):
+    return word.lower() in english_words
 
 if __name__ == '__main__':
     ## api_key, *image_filenames = argv[1:]
     api_key = 'AIzaSyC15tABjgjnGRO4pshXCrH7ONlgTgGXWHk'
-    image_filenames = ['a.jpg']
+    image_filenames = ['Images/13.jpg']
 
     if not api_key or not image_filenames:
         print("""
@@ -67,11 +100,18 @@ if __name__ == '__main__':
                     datatxt = json.dumps(resp, indent=2)
                     print("Wrote", len(datatxt), "bytes to", jpath)
                     f.write(datatxt)
-
-                # print the plaintext to screen for convenience
-                print("---------------------------------------------")
                 t = resp['textAnnotations'][0]
-                print("    Bounding Polygon:")
-                print(t['boundingPoly'])
-                print("    Text:")
-                print(t['description'])
+                result = t['description']
+                result = removeNonAscii(result)
+                print("Original Sample:")
+                print(result)
+                result = result.replace("\n", " ")
+                result = result.split(" ")
+                result = [word for word in result if not hasRandom(word)]
+                result = [word for word in result if is_word(word)]
+                result = ' '.join(result)
+                print()
+                print("Sanitized Sample:")
+                print(result)
+                print()
+                print()
