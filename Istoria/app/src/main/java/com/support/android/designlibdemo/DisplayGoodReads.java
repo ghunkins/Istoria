5e2230523272c7d50668a95188548cb358755ad7
Brandon Slaght <brandonsesipod@gmail.com>
2017-02-19 07:38:40 -0500

    fix html formatting in synopsis

--- Istoria/app/src/main/java/com/support/android/designlibdemo/DisplayGoodReads.java
+++ Istoria/app/src/main/java/com/support/android/designlibdemo/DisplayGoodReads.java
@@ -60,7 +60,11 @@ public class DisplayGoodReads extends AppCompatActivity {
         author = intent.getStringExtra(EXTRA_AUTHOR);
         ratingText = intent.getStringExtra(EXTRA_RATING);
         synopsis = intent.getStringExtra(EXTRA_SYNOPSIS);
+        synopsis = android.text.Html.fromHtml(synopsis).toString();
         image = intent.getStringExtra(EXTRA_IMAGE);
+
+        String test = "<strong>Harry</strong><strong> Potter's life is miserable. His parents are dead and he's stuck with his heartless relatives, who force him to live in a tiny closet under the stairs. But his fortune changes when he receives a letter that tells him the truth about himself: he's a wizard. A mysterious visitor rescues him from his relatives and takes him to his new home, Hogwarts School of Witchcraft and Wizardry.<br /><br />After a lifetime of bottling up his magical powers, Harry finally feels like a normal kid. But even within the Wizarding community, he is special. He is the boy who lived: the only person to have ever survived a killing curse inflicted by the evil Lord Voldemort, who launched a brutal takeover of the Wizarding world, only to vanish after failing to kill Harry.<br /><br />Though Harry's first year at Hogwarts is the best of his life, not everything is perfect. There is a dangerous secret object hidden within the castle walls, and Harry believes it's his responsibility to prevent it from falling into evil hands. But doing so will bring him into contact with forces more terrifying than he ever could have imagined.<br /><br />Full of sympathetic characters, wildly imaginative situations, and countless exciting details, the first installment in the series assembles an unforgettable magical world and sets the stage for many high-stakes adventures to come.";
+        Log.i("test", android.text.Html.fromHtml(test).toString());
 
         // load the toolbars
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);   //finds the toolbar
